f05c765849d5db19cadfcc079ae33ea7dd2f66a1
Triple the Boba ! <coconuttt1010@gmail.com>
2021-12-02 11:00:51 -0800

    syntax

---
+++ client.js
@@ -0,0 +1,30 @@
+/**
+ * Local Server Play - npm run play
+ * IP addresss: localhost
+ */
+
+const net = require("net");
+// establishes a connection with the game server
+const connect = () => {
+  /**
+   *  using Node's net library (createConnection() func)
+   * -> create an object named conn
+   * conn obj has useful methods and properties to interact with the server
+   */
+  const conn = net.createConnection({ // the connection with the server
+    host: "localhost",  // IP address here,
+    port: 50541         // PORT number here,
+  });
+  conn.setEncoding("utf8"); // interpret incoming data as text
+
+  conn.on("connect", () => {
+    console.log("Connecting...");
+  });
+  conn.on("close", () => {
+    console.log("U ded cuz you idled! OUT!");
+  });
+
+  return conn;
+};
+
+module.exports = {connect}; // exports using es6 syntax
